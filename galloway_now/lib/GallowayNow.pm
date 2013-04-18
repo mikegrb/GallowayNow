d2cab5370cbda686608aae630a537699b5d715ee
mikegrb <mgreb@linode.com>
2013-04-17 20:20:18 -0400

    Show radio metadata log via ajax updates
    Could use some smarts, keep track of mtime and only send if there's new stuff, etc

--- galloway_now/lib/GallowayNow.pm
+++ galloway_now/lib/GallowayNow.pm
@@ -4,18 +4,23 @@ use Mojo::Base 'Mojolicious';
 use DBI;
 use Sys::Hostname;
 
-our $notices_db = '/home/michael/public_html/public_notices/notices.db';
+our $notices_db   = '/home/michael/public_html/public_notices/notices.db';
+our $archive_path = '/home/michael/public_html/stream/archive';
+
+if ( hostname() ne 'orion' ) {
+    $notices_db
+        = '/Users/mgreb/Dropbox/Documents/proj/public_notice/notices.db';
+    $archive_path = '/Users/mgreb/Dropbox/Documents/proj/scanner_archive/test';
+}
 
 sub startup {
     my $self = shift;
-
-    $notices_db = '/Users/mgreb/Dropbox/Documents/proj/public_notice/notices.db'
-        if hostname() eq 'soryu2.linlan';
 
     my $r = $self->routes;
 
     $r->get('/')->to( 'Main#index', active_tab => 1 );
     $r->get('/radio/')->to( 'Radio#index', active_tab => 2 );
+    $r->get('/radio/log')->to('Radio#GetLog');
     $r->get('/outages/')->to( 'Outages#index',       active_tab => 3 );
     $r->get('/notices/')->to( 'PublicNotices#index', active_tab => 4 );
     $r->get('/notices/:page')->to( 'PublicNotices#index', active_tab => 4 );
--- galloway_now/lib/GallowayNow/Radio.pm
+++ galloway_now/lib/GallowayNow/Radio.pm
@@ -1,9 +1,29 @@
 package GallowayNow::Radio;
+
+use POSIX 'strftime';
 use Mojo::Base 'Mojolicious::Controller';
 
 sub index {
     my $self = shift;
     $self->render();
+}
+
+sub GetLog {
+    my $self = shift;
+    my $path = $GallowayNow::archive_path
+        . strftime( '/%Y/%m/%d/log.txt', localtime );
+    my $data = tail($path);
+    $self->render( text => $data, format => 'txt' );
+}
+
+sub tail {
+    my $path = shift;
+    my $lines = shift || 5;
+
+    open( my $fh, '-|', '/usr/bin/tail', '-n', $lines, $path ) or return;
+    my @lines = <$fh>;
+    close($fh);
+    return join '', @lines;
 }
 
 1;
