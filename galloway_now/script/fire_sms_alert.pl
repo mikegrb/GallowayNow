f6c3008beaee005fa321dfb0b0a1a1c4a2ad373b
mikegrb <mgreb@linode.com>
2013-05-21 20:33:01 -0400

    Move rest of config for fire sms alerts to conf, rename conf

--- galloway_now/script/fire_sms_alert.pl
+++ galloway_now/script/fire_sms_alert.pl
@@ -12,22 +12,20 @@ use Config::Auto;
 use POSIX 'strftime';
 use WWW::Twilio::API;
 
-my $touch_file     = '/tmp/gnow_twilio_fire';
-my $sleep_time     = 5;                         # min 5 min between sms
-my $log_lines      = 20;                        # check last 20 lines of logs
-my $required_count = 3;                         # require 3 matches
+my $config = Config::Auto::parse("$FindBin::Bin/../../conf/fire_sms_aler.conf");
 
 exit 10
-    if -e $touch_file && ( stat $touch_file )[9] > time - ( $sleep_time * 60 );
+    if -e $config->{touch_file}
+    && ( stat $config->{touch_file} )[9]
+    > time - ( $config->{sleep_time} * 60 );
 
-my $config = Config::Auto::parse("$FindBin::Bin/../../conf/twilio.conf");
 my $archive_path
     = strftime( "$GallowayNow::archive_path/%Y/%m/%d/log.txt", localtime );
 
 chomp( my $count = `tail -n $log_lines $archive_path | grep -ci fire` );
 
-if ( $count < $required_count ) {
-    say "$count lines with fire in last 10 lines of $archive_path";
+if ( $count < $config->{required_count} ) {
+#   say "$count lines with fire in last 10 lines of $archive_path";
     exit 0;
 }
 
