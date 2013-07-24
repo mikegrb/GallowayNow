e38b67e2b9c6aa485b721ce22808710fb5a580b8
mikegrb <mgreb@linode.com>
2013-07-24 01:09:42 -0400

    completed gtv scraping, need to figure out pretty-ish output and TOS/copyright before posting to gallowaynow.com

--- in-progress/scrape-gtv.pl
+++ in-progress/scrape-gtv.pl
@@ -2,11 +2,14 @@
 
 use strict;
 use warnings;
+use autodie;
 
 use 5.014;
 
 use Mojo::UserAgent;
 use Data::Printer;
+
+$| = 1;
 
 my $ua = Mojo::UserAgent->new;
 
@@ -14,44 +17,27 @@ my $items
     = $ua->get('http://webus.telvue.com/wi/index.aspx?cid=39')
     ->res->dom->at('#MessageSelect')
     ->children->map(
-    sub { $_->text =~ m/^- / ? [ $_->attrs('value'), $_->text ] : () } );
+    sub { $_->text =~ m/^- (.*)$/ ? [ $_->attrs('value'), $1 ] : () } );
 
-p $items;
+open my $index, '>', 'output/index.html';
 
-__END__
-
-http://webus.telvue.com/wi/index.aspx?cid=39
-
-
-
-items is now a Mojo::Collection that looks like:
-
-[0]  [
-    [0] "39:8140:17453",
-    [1] "- THE GALLOWAY TOWNSHIP ENVIRONMENTAL COMMISSION"
-],
-[1]  [
-    [0] "39:506493:861326",
-    [1] "- ONLY RAIN DOWN THE DRAIN"
-],
-[2]  [
-    [0] "39:901865:1468592",
-    [1] "- AFFORDABLE HOUSING UNITS"
-],
-[3]  [
-    [0] "39:499560:849554",
-    [1] "- Mid Atlantic AARP"
-],
-
-
-Next step is hit url for each message:
-cid,mid,mpi are the first element split on :
-http://webus.telvue.com/wi/index.aspx?cid=39&mid=630683&mpi=1051652&pno=1
-
-
-on that page for each item #message-pages contains an A href for each page in
-that message
-
-message page thumbnails are then:
-pno == page number
-http://webus.telvue.com/ThumbnailPreview/GetThumbnail.aspx?weblinx=true&mid=630683&mpi=1051652&pno=1
+for my $item (@$items) {
+    print "Grabbing $item->[1]... ";
+    my ( $cid, $mid, $mpi ) = split /:/, $item->[0];
+    my $url = "http://webus.telvue.com/wi/index.aspx?cid=$cid&mid=$mid&mpi=$mpi";
+    my $pages = $ua->get("${url}&pno=1")->res->dom->at('#message-pages')->children->size;
+    
+    print "$pages pages... ";
+    
+    say $index "<h2>$item->[1]</h3>";
+    for my $page ( 1 .. $pages ) {
+        print "$page ";
+        my $filename = "$mid-$mpi-$page.png";
+        my $url = "http://webus.telvue.com/ThumbnailPreview/GetThumbnail.aspx?" 
+        . "weblinx=true&mid=$mid&mpi=$mpi&pno=$page";
+        $ua->get($url)->res->content->asset->move_to("output/$filename");
+        say $index qq{<img src="$filename" width="400" height="300"/>};
+    }
+    say $index "<hr/>";
+    print "\n";
+}
