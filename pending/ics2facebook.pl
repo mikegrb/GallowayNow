739f62a3de1e4a6a5725c62fff9dc7aabe5bc0b3
mikegrb <mgreb@linode.com>
2013-10-13 00:59:42 -0400

    beginnings of ics -> facebook Based on the 2nd and 3rd recipes in the examples dir from Facebook::Graph.  Also adds basic tz needful.
    Works towards #16

---
+++ pending/ics2facebook.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+
+use 5.012;
+
+use Facebook::Graph;
+use Config::JSON;
+use Ouch;
+
+use List::Util 'first';
+use Data::Dumper;
+use Data::Printer;
+use Data::ICal;
+use LWP::UserAgent;
+use DateTime::Format::ICal;
+
+my $fb_config = Config::JSON->new('my.conf')->get('ics2facebook');
+my $fb        = Facebook::Graph->new($fb_config);
+
+# get access token for the page
+my $pages = $fb->query->find('me/accounts')
+    ->include_metadata->request->as_hashref->{data};
+my $page = first { $_->{name} eq 'Galloway Now' } @$pages;
+$fb->access_token( $page->{access_token} );
+
+# grab and parse ical
+my $ical
+    = LWP::UserAgent->new->get(
+    'http://www.google.com/calendar/ical/ivgf5vo85m6t7daacoj4ei32e8%40group.calendar.google.com/public/basic.ics'
+    )->content;
+my $calendar = Data::ICal->new( data => $ical );
+
+# post events
+foreach my $entry ( @{ $calendar->entries } ) {
+    next unless $entry->properties->{summary}[0]{value};
+    for my $time ( 'dtstart', 'dtend' ) {
+        next if $entry->properties->{$time}[0]{value} =~ /Z$/;
+        $entry->properties->{$time}[0]{value}
+            = 'TZID=America/New_York:' . $entry->properties->{$time}[0]{value};
+    }
+
+    say $entry->properties->{summary}[0]{value};
+    print Dumper $fb->add_event->set_name(
+        $entry->properties->{summary}[0]{value} )
+        ->set_location( $entry->properties->{location}[0]{value} )
+        ->set_description( $entry->properties->{description}[0]{value} )
+        ->set_start_time( DateTime::Format::ICal->parse_datetime( $entry->properties->{dtstart}[0]{value} ) )
+        ->set_end_time( DateTime::Format::ICal->parse_datetime( $entry->properties->{dtend}[0]{value} ) )
+        ->publish;
+}
+
